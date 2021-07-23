59676b507ce49c7f4c7d6b61e56cdf8f3aa7c1e1
rubiocode <78938193+rubiocode@users.noreply.github.com>
2021-07-22 20:54:21 -0700

    added Department function coded and working properly

--- index.js
+++ index.js
@@ -22,12 +22,27 @@ connection.connect(async (err)=>{
             {
                 name: 'startMenu',
                 type: 'list',
-                message: 'Welcome, please select and item to continue',
+                message: 'Welcome, please select an item to continue',
                 choices: [
-                    'Department',
-                    'Employee',
-                    'Roles',
-                    'Budget',
+                    new inquirer.Separator('───────────────────── Add ──────────────────────'),
+                    'Add Department',
+                    'Add Employee',
+                    'Add Role',
+                    new inquirer.Separator('──────────────────── Delete ────────────────────'),
+                    'Delete Department',
+                    'Delete Employee',
+                    'Delete Role',
+                    new inquirer.Separator('───────────────────── View ─────────────────────'),
+                    'View All Departments',
+                    'View All Employees',
+                    'View All Roles',
+                    'View Employees by Manager',
+                    'View Department\'s Utilized Budget',
+                    new inquirer.Separator('──────────────────── Update ────────────────────'),
+                    'Update Employee Role',
+                    'Update Employee Manager',
+                    new inquirer.Separator('───────────────────── Exit ─────────────────────'),
+                    'Exit Node.js'
                 ],
             },
         ]);
@@ -38,18 +53,107 @@ connection.connect(async (err)=>{
     };
 });
 
-const selectedItem = async (userSelection) => {
-    if (userSelection === 'Department'){
-
+    const selectedItem = async (userSelection) => {
+    if (userSelection === 'Add Department'){
+        addDepartment();
     };
-    if (userSelection === 'Employee'){
-
+    /*if (userSelection === 'Add Employee'){
+        addEmployee();
     };
-    if (userSelection === 'Roles'){
-
+    if (userSelection === 'Add Role'){
+        addRole();
     };
-    if (userSelection === 'Budget'){
-
+    if (userSelection === 'Delete Department'){
+        deleteDepartment();
     };
+    if (userSelection === 'Delete Employee'){
+        deleteEmployee();
+    };
+    if (userSelection === 'Delete Role'){
+        deleteRole();
+    };
+    if (userSelection === 'View All Departments'){
+        viewDepartment();
+    };
+    if (userSelection === 'View All Employees'){
+        viewEmployee();
+    };
+    if (userSelection === 'View All Roles'){
+        viewRoles();
+    };
+    if (userSelection === 'View Employees by Manager'){
+        viewEmployeeByManager();
+    };
+    if (userSelection === 'View Department\'s Utilized Budget'){
+        viewTotalUsedBudget();
+    };
+    if (userSelection === 'Update Employee Role'){
+        updateRole();
+    };
+    if (userSelection === 'Update Employee Manager'){
+        updateManager();
+    };
+    if (userSelection === 'Exit Node.js'){
+        exitMenu();
+    }*/
+};
 
+const addDepartment = async ()=> {
+    try {
+        const  userResponse = await inquirer.prompt ([
+            {
+                name: 'department',
+                type: 'input',
+                message: 'What is the name of the deparment?',
+            },
+        ]);
+        
+        const query = `INSERT INTO department VALUES ('${userResponse.department}')`;
+
+        connection.query(query, userResponse, (err, result)=>{
+            if (err) throw err;
+            console.log('YOUR ADDED DEPARTMENT IS', result);
+            connection.end();
+        })
+
+    } catch (e) {
+        connection.end();
+    }
 }
+
+const addEmployee = async ()=>{
+    try {
+    
+    } catch (e) {
+    
+    }
+}
+
+/*{
+            name: 'addEmployeeFirst',
+            type: 'input',
+            message: 'What is the first name of the employee?',
+        },
+        {
+            name: 'addEmployeeLast',
+            type: 'input',
+            message: 'What is the last name of the employee?',
+        },
+        {
+            name: 'addManager',
+            type: 'input',
+            message: 'What is the name of the employee\'s manager?',
+        },
+        {
+            name: 'addTitle',
+            type: 'input',
+            message: 'What is the employee\'s title?',
+        },
+        {
+            name: 'addSalary',
+            type: 'input',
+            message: 'What is the employee\'s salary?',
+        },
+
+    ]);*/
+    
